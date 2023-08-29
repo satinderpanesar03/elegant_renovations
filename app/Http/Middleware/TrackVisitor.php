2596b8eff66110f4be674b5fcc2237c39666e7d6
Hence forth <vicky.chhetri.henceforth@gmail.com>
2023-08-29 17:32:53 +0530

    tracker kernal

---
+++ app/Http/Middleware/TrackVisitor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrackVisitor
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Create a new Visitor entry in the database
+        Visitor::create([
+            'ip_address' => $request->ip(),
+            'user_agent' => $request->header('User-Agent'),
+        ]);
+
+        return $next($request);
+    }
+}
