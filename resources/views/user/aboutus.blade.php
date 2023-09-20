0679b6903d78498047a87c8568b1f964f9759ccf
Vicky Chhetri <specialpower321@gmail.com>
2023-09-21 00:49:47 +0530

    about us page

--- resources/views/user/aboutus.blade.php
+++ resources/views/user/aboutus.blade.php
@@ -8,13 +8,13 @@
             <div class="about-us-page header-content-wrapper">
                 <div class="about-us-page header-content">
                     <div class="about-us-page header-content-detail">
-                        <h2>ABOUT US</h2>                        
+                        <h2>ABOUT US</h2>
                 </div>
             </div>
         </header>
         <div class="trusted-kitchen-renovation-section-wrapper">
             <div class="trusted-kitchen-renovation-section-heading">
-                <h2>Trusted Kitchen Renovation & Cabinets Company in Calgary & Edmonton</h2>
+                <h2> EXPERTISE & PASSION AT THE FOREFRONT OF HOME DEVELOPMENT & RENOVATION</h2>
                 <div class="below-heading-line"></div>
             </div>
             <div class="trusted-kitchen-renovation-section-content">
@@ -33,27 +33,39 @@
                     </div>
                 </div>
                 <div class="trusted-kitchen-renovation-section-content-desc col-md-12 col-lg-5 col-xl-6">
-                    <h5>Kitchen Design & kitchen Remodeling Experts</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, inventore debitis illo veniam explicabo impedit quidem ea suscipit eum et quia placeat deserunt nihil fugit velit sapiente vero accusantium dolorum quo optio molestias eveniet fuga a omnis?  Illum corrupti distinctio  </p>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet, esse illum dolorem voluptates enim delectus eius fuga placeat aspernatur laboriosam.</p>
-                    <br>
-                    <h5>Below are some reasons to renovate your kitchen:</h5>
+                    <h5>Elevate Your Living Spaces with AMKA Homes: Your Trusted Home Renovation Partner</h5>
+                    <p style="text-align: justify;">AMKA Homes LTD is a leading home development and remodeling company, founded by Amit Arora and Kamal Dhingra, two highly skilled and experienced contractors with decades of industry experience. Our team is composed of industry experts, including architects, designers, and project managers, all of whom are dedicated to providing top-notch development and transformations to residential properties of all types.</p>
+
+                    <p style="text-align: justify;">
+                        We specialize in creating custom and personalized living spaces, from apartments to villas and modern contemporary to traditional homes. Our team has the skills, experience, and expertise to bring your dream home to life, utilizing the latest construction techniques and materials to ensure that your renovation project is completed to the highest standards.
+                    </p>
+                    <p style="text-align: justify;">
+                        At AMKA Homes LTD, we believe in open communication and collaboration with our clients throughout the entire renovation process, ensuring a seamless and stress-free experience. Our mission is to provide our clients with the highest level of craftsmanship and professionalism, resulting in exceptional and personalized living spaces that exceed their expectations. Trust us with your next home renovation project and see the difference that our expertise and commitment to excellence can make.
+                    </p>
+                    <h5>Below are some reasons to renovate your home:</h5>
                     <ul>
                         <li>
-                            <img src="./images/success-tick.png.png" alt="">
-                            <p>Update the aesthetics</p>
+
+                            <p>Enhance Your Quality of Life</p>
                         </li>
                         <li>
-                            <img src="./images/success-tick.png.png" alt="">
-                            <p>Improve overall kitchen functionality</p>
+
+                            <p>Increase Property Value</p>
                         </li>
                         <li>
-                            <img src="./images/success-tick.png.png" alt="">
-                            <p>Boost home value</p>
+
+                            <p>Personalization</p>
+                        </li>
+                        <li>
+
+                            <p>Energy Efficiency</p>
+                        </li>
+                        <li>
+
+                            <p>Maintenance and Repairs</p>
                         </li>
                     </ul>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid eaque optio quia, magnam aliquam beatae architecto obcaecati, itaque quidem molestias, accusantium laudantium sunt impedit neque sint. Dolorem animi quia labore unde libero aspernatur? Architecto corporis harum dicta nisi mollitia? Consectetur nulla minima hic perspiciatis.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora expedita saepe, iure quae corporis illo alias est natus distinctio voluptatem?</p>
+                    <p style="text-align: justify;">AMKA Homes is here to transform your house into your dream home, delivering exceptional craftsmanship and professionalism at every step of the renovation process. Trust us to bring your vision to life, ensuring that your newly renovated space exceeds your expectations.</p>
                 </div>
             </div>
         </div>
@@ -67,38 +79,38 @@
             </div>
             <div class="about-us-why-choose-us-section-content">
                 <div class="about-us-why-choose-us-section-content-desc ">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse ipsum, incidunt nesciunt soluta quasi praesentium repudiandae sunt accusamus reprehenderit veritatis. Esse ipsum, incidunt nesciunt soluta quasi praesentium repudiandae sunt accusamus reprehenderit veritatis.</p>
+                    <p>There are several compelling reasons to choose AMKA Homes for your home development and remodeling needs:</p>
                 </div>
                 <div class="about-us-why-choose-us-section-content-cards">
                     <div class="about-us-why-choose-us-section-content-cards-item col-md-4 col-11">
                         <img src="./images/success-tick.png.png" alt="">
-                         <h5 class="mb-3">Balanced Kitchen Design</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore vel dolore veniam eligendi. Rem ullam explicabo commodi error, illo nam quaerat corporis, et saepe iure perferendis a praesentium? Eum soluta magnam, vero facilis optio corporis hic. Id doloremque maxime dicta.</p>
+                         <h5 class="mb-3">Expertise and Experience</h5>
+                        <p style="text-align: justify;">Our company was founded by Amit Arora and Kamal Dhingra, industry veterans with decades of experience in construction and remodeling. We bring a wealth of knowledge to every project.</p>
                     </div>
                     <div class="about-us-why-choose-us-section-content-cards-item col-md-4 col-11">
                         <img src="./images/success-tick.png.png" alt="">
-                         <h5 class="mb-3">Co-ordination with Tradespeople</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore vel dolore veniam eligendi. Rem ullam explicabo commodi error, illo nam quaerat corporis, et saepe iure perferendis a praesentium? Eum soluta magnam, vero facilis optio corporis hic. Id doloremque maxime dicta.</p>
+                         <h5 class="mb-3">Skilled Team</h5>
+                        <p style="text-align: justify;">Our team is composed of skilled professionals, including architects, designers, and project managers, all working together to deliver exceptional results. We hire the best talent in the industry to ensure your project's success.</p>
                     </div>
                     <div class="about-us-why-choose-us-section-content-cards-item col-md-4 col-11">
                         <img src="./images/success-tick.png.png" alt="">
                          <h5 class="mb-3">Installation Timelines</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore vel dolore veniam eligendi. Rem ullam explicabo commodi error, illo nam quaerat corporis, et saepe iure perferendis a praesentium? Eum soluta magnam, vero facilis optio corporis hic. Id doloremque maxime dicta.</p>
+                        <p style="text-align: justify;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore vel dolore veniam eligendi. Rem ullam explicabo commodi error, illo nam quaerat corporis, et saepe iure perferendis a praesentium? Eum soluta magnam, vero facilis optio corporis hic. Id doloremque maxime dicta.</p>
                     </div>
                     <div class="about-us-why-choose-us-section-content-cards-item col-md-4 col-11">
                         <img src="./images/success-tick.png.png" alt="">
-                         <h5 class="mb-3">Upfront & Transparent Pricing</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore vel dolore veniam eligendi. Rem ullam explicabo commodi error, illo nam quaerat corporis, et saepe iure perferendis a praesentium? Eum soluta magnam, vero facilis optio corporis hic. Id doloremque maxime dicta.</p>
+                         <h5 class="mb-3">Customization</h5>
+                        <p style="text-align: justify;">We specialize in creating custom and personalized living spaces. No matter the type of property or your style preferences, we have the expertise to bring your unique vision to life.</p>
                     </div>
                     <div class="about-us-why-choose-us-section-content-cards-item col-md-4 col-11">
                         <img src="./images/success-tick.png.png" alt="">
-                         <h5 class="mb-3">Consultation & Guidance</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore vel dolore veniam eligendi. Rem ullam explicabo commodi error, illo nam quaerat corporis, et saepe iure perferendis a praesentium? Eum soluta magnam, vero facilis optio corporis hic. Id doloremque maxime dicta.</p>
+                         <h5 class="mb-3">Quality and Craftsmanship</h5>
+                        <p style="text-align: justify;">We are committed to the highest standards of quality and craftsmanship. We use the latest construction techniques and materials to ensure that your renovation project is completed to perfection.</p>
                     </div>
                     <div class="about-us-why-choose-us-section-content-cards-item col-md-4 col-11">
                         <img src="./images/success-tick.png.png" alt="">
-                         <h5 class="mb-3">Custom Cabinetry</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore vel dolore veniam eligendi. Rem ullam explicabo commodi error, illo nam quaerat corporis, et saepe iure perferendis a praesentium? Eum soluta magnam, vero facilis optio corporis hic. Id doloremque maxime dicta.</p>
+                         <h5 class="mb-3">Open Communication</h5>
+                        <p style="text-align: justify;">We believe in open and transparent communication with our clients throughout the entire renovation process. We keep you informed at every stage, ensuring a seamless and stress-free experience.</p>
                     </div>
                 </div>
             </div>
@@ -437,7 +449,7 @@
 
     <!--  What are customers says about us end -->
 
-           
+
         <!-- latest-blog-posts-section start here -->
         <div class="latest-blog-posts-section-wrapper">
             <div class="latest-blog-posts-section-heading">
