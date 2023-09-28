56d0cdbda231e3f35595e8b1e1eb4dee69b73175
Hence forth <vicky.chhetri.henceforth@gmail.com>
2023-09-28 11:23:54 +0530

    design

--- resources/views/components/user/recentdesign.blade.php
+++ resources/views/components/user/recentdesign.blade.php
@@ -5,24 +5,25 @@
         <div class="below-heading-line"></div>
     </div>
     <div class="our-recent-kitchens-design-section-content">
-        @foreach($designData as $design)
+        @if($designData!=null)
+        @foreach($designData as $designItem)
         <div class="our-recent-kitchens-design-section-content-card-1">
             <div class="img-wrapper" >
-            <img src="{{asset('storage/'.$design->image)}}" class="img-fluid">
+            <img src="{{asset('storage/'.$designItem->image)}}" class="img-fluid">
             </div>
             <div class="detail-wrapper">
                 <div class="desc-wrapper">
-                    <p>{{$design->title}}</p>
-                    <label>{{$design->meta_description}}</label>
+                    <p>{{$designItem->title}}</p>
+                    <label>{{$designItem->meta_description}}</label>
                 </div>
                 <div class="btn-wrapper">
-                    <p><a href="{{route('user.recent.design.listing', $design->id)}}" class="text-decoration-none text-dark"> See the design</a></p>
+                    <p><a href="{{route('user.recent.design.listing', $designItem->id)}}" class="text-decoration-none text-dark"> See the design</a></p>
                     <img src="{{asset('user/images/orange-arrow.png.png')}}" alt="">
                 </div>
             </div>
         </div>{{asset('user/images/orange-arrow.png.png')}}
         @endforeach
-
+        @endif
         <div class="our-recent-kitchens-design-section-content-btn-wrapper">
             <button>View more</button>
         </div>
