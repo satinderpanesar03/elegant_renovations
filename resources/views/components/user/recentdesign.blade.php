2072ef5cd03c0b682954e38a23b55b607b4837f6
Hence forth <vicky.chhetri.henceforth@gmail.com>
2023-09-29 13:17:02 +0530

    design header in h4

--- resources/views/components/user/recentdesign.blade.php
+++ resources/views/components/user/recentdesign.blade.php
@@ -17,7 +17,7 @@
                                             </div>
                                     <div class=" ">
                                                 <div class="desc-wrapper">
-                                                    <p class="font-weight-bold p-2 mt-3">{{$designItem->title}}</p>
+                                                    <h4 class="font-weight-bold p-2 mt-3">{{$designItem->title}}</h4>
                                                     <label>{{substr($designItem->meta_description, 0, 100)}}...</label>
                                                 </div>
                                                 <div class="btn-wrapper">
