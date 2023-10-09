36316b0119b281c885815925524c512370baa6ae
satinderpal <satindepanesar03@gmail.com>
2023-10-09 17:56:21 +0530

    =fd

--- resources/views/components/user/getquote.blade.php
+++ resources/views/components/user/getquote.blade.php
@@ -5,25 +5,25 @@
     <form action="{{route('user.contact.us.store')}}" method="post">
         @csrf
         <div class="get-a-free-quote-section-content">
-            <p>Contact our Kitchen renovation experts serving calgary, edmonton & vancouver</p>
+            <p class="mb-3">Contact our Kitchen renovation experts serving calgary, edmonton & vancouver</p>
             <form>
                 <div class="row">
                     <div class="col">
-                        <input type="text" class="form-control" placeholder="First name" name="firstname">
+                        <input type="text" class="form-control mb-3" placeholder="First name" name="firstname">
                     </div>
                     <div class="col">
-                        <input type="text" class="form-control" placeholder="last name" name="lastname">
+                        <input type="text" class="form-control mb-3" placeholder="last name" name="lastname">
                     </div>
                 </div>
                 <div class="row">
                     <div class="col">
-                        <input type="text" class="form-control" placeholder="Phone no" name="phone">
+                        <input type="text" class="form-control mb-3" placeholder="Phone no" name="phone">
                     </div>
                     <div class="col">
-                        <input type="text" class="form-control" placeholder="E-mail" name="email">
+                        <input type="text" class="form-control mb-3" placeholder="E-mail" name="email">
                     </div>
                 </div>
-                <textarea class="form-control" placeholder="Enter your message" rows="5" id="comment" name="text"></textarea>
+                <textarea class="form-control mb-3" placeholder="Enter your message" rows="5" id="comment" name="text"></textarea>
 
             </form>
             <button type="submit">Submit</button>
