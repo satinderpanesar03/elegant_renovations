d43b7714c934b0de0509c5bffa9803b3e77b32d5
satinderpal <satindepanesar03@gmail.com>
2023-10-09 17:53:48 +0530

    dffs

--- resources/views/components/user/getquote.blade.php
+++ resources/views/components/user/getquote.blade.php
@@ -2,29 +2,31 @@
     <div class="get-a-free-quote-section-heading">
         <h2>Get A Free Quote</h2>
     </div>
-    <div class="get-a-free-quote-section-content">
-        <p>Contact our Kitchen renovation experts serving calgary, edmonton & vancouver</p>
-        <form>
-            <div class="row">
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="First name" name="firstname">
+    <form action="{{route('user.contact.us.store')}}" method="post">
+        @csrf
+        <div class="get-a-free-quote-section-content">
+            <p>Contact our Kitchen renovation experts serving calgary, edmonton & vancouver</p>
+            <form>
+                <div class="row">
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="First name" name="firstname">
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="last name" name="lastname">
+                    </div>
                 </div>
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="last name" name="lastname">
+                <div class="row">
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Phone no" name="phone">
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="E-mail" name="email">
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Phone no" name="phone">
-                </div>
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="E-mail" name="email">
-                </div>
-            </div>
-            <textarea class="form-control"  placeholder="Enter your message" rows="5" id="comment"
-                      name="text"></textarea>
+                <textarea class="form-control" placeholder="Enter your message" rows="5" id="comment" name="text"></textarea>
 
-        </form>
-        <button type="submit">Submit</button>
-    </div>
+            </form>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
 </div>
