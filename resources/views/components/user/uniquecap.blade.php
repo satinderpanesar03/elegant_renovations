ec7593a431be83e0d2739912f066e1a9f4d30307
Vicky Chhetri <specialpower321@gmail.com>
2023-11-06 12:47:38 +0530

    regfrelext changes

--- resources/views/components/user/uniquecap.blade.php
+++ resources/views/components/user/uniquecap.blade.php
@@ -1,59 +1,5 @@
 <div class="reflect-your-sense-of-style-section-wrapper">
     <div class="reflect-your-sense-of-style-section-heading">
-        <h2> Capture Your Unique Style and Transform Your Dreams into Tangible Reality</h2>
-        <div class="below-heading-line"></div>
-    </div>
-    <div class="reflect-your-sense-of-style-section-content">
-        <div class="reflect-your-sense-of-style-section-content-images col-md-12 col-lg-6">
-            <div class="d-flex">
-                <div class="Kitchen-Design-img-1 Kitchen-design-wrapper">
-                </div>
-                <div class="Kitchen-Design-img-2 Kitchen-design-wrapper">
-                </div>
-            </div>
-            <div class="d-flex">
-                <div class="Kitchen-Design-img-3 Kitchen-design-wrapper">
-                </div>
-                <div class="Kitchen-Design-img-4 Kitchen-design-wrapper">
-                </div>
-            </div>
-        </div>
-        <div class="reflect-your-sense-of-style-section-content-desc col-md-12 col-lg-6">
-            <div class="desc-1 desc-wrapper">
-                <div><img src="{{asset('user/images/success-tick.png.png')}}" alt=""></div>
-                <div>
-                    <h5>All-in-One Experience</h5>
-                    <p> Experience a seamless kitchen renovation journey with our all-in-one service. Our experts handle everything from start to finish, ensuring a stress-free process and remarkable results that exceed your expectations.</p>
-                </div>
-            </div>
-            <div class="desc-2 desc-wrapper">
-                <div><img src="{{asset('user/images/success-tick.png.png')}}" alt=""></div>
-                <div>
-                    <h5>Stunning Design</h5>
-                    <p> Transform your kitchen into a masterpiece with our captivating designs. Our team blends beauty and functionality, creating a space that's visually striking and incredibly practical, making cooking a joy and entertaining a breeze.</p>
-                </div>
-            </div>
-            <div class="desc-3 desc-wrapper">
-                <div><img src="{{asset('user/images/success-tick.png.png')}}" alt=""></div>
-                <div>
-                    <h5>Quality Materials</h5>
-                    <p> Discover a kitchen built to last. We source the finest materials, from sturdy cabinetry to elegant countertops, ensuring your renovated kitchen is as durable as it is stunning.</p>
-                </div>
-            </div>
-            <div class="desc-btn-wrapper">
-                <button >  <a href="{{route('user.contact.us')}}" class="text-decoration-none text-white"    >Get a Quote</a></button>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-{{-- new construction --}}
-
-
-<div class="reflect-your-sense-of-style-section-wrapper">
-    <div class="reflect-your-sense-of-style-section-heading">
         <h2>New Construction: Crafting Your Vision into Reality</h2>
         <div class="below-heading-line"></div>
     </div>
@@ -118,3 +64,56 @@
 
     </div>
 </div>
+
+<div class="reflect-your-sense-of-style-section-wrapper">
+    <div class="reflect-your-sense-of-style-section-heading">
+        <h2> Capture Your Unique Style and Transform Your Dreams into Tangible Reality</h2>
+        <div class="below-heading-line"></div>
+    </div>
+    <div class="reflect-your-sense-of-style-section-content">
+        <div class="reflect-your-sense-of-style-section-content-images col-md-12 col-lg-6">
+            <div class="d-flex">
+                <div class="Kitchen-Design-img-1 Kitchen-design-wrapper">
+                </div>
+                <div class="Kitchen-Design-img-2 Kitchen-design-wrapper">
+                </div>
+            </div>
+            <div class="d-flex">
+                <div class="Kitchen-Design-img-3 Kitchen-design-wrapper">
+                </div>
+                <div class="Kitchen-Design-img-4 Kitchen-design-wrapper">
+                </div>
+            </div>
+        </div>
+        <div class="reflect-your-sense-of-style-section-content-desc col-md-12 col-lg-6">
+            <div class="desc-1 desc-wrapper">
+                <div><img src="{{asset('user/images/success-tick.png.png')}}" alt=""></div>
+                <div>
+                    <h5>All-in-One Experience</h5>
+                    <p> Experience a seamless kitchen renovation journey with our all-in-one service. Our experts handle everything from start to finish, ensuring a stress-free process and remarkable results that exceed your expectations.</p>
+                </div>
+            </div>
+            <div class="desc-2 desc-wrapper">
+                <div><img src="{{asset('user/images/success-tick.png.png')}}" alt=""></div>
+                <div>
+                    <h5>Stunning Design</h5>
+                    <p> Transform your kitchen into a masterpiece with our captivating designs. Our team blends beauty and functionality, creating a space that's visually striking and incredibly practical, making cooking a joy and entertaining a breeze.</p>
+                </div>
+            </div>
+            <div class="desc-3 desc-wrapper">
+                <div><img src="{{asset('user/images/success-tick.png.png')}}" alt=""></div>
+                <div>
+                    <h5>Quality Materials</h5>
+                    <p> Discover a kitchen built to last. We source the finest materials, from sturdy cabinetry to elegant countertops, ensuring your renovated kitchen is as durable as it is stunning.</p>
+                </div>
+            </div>
+            <div class="desc-btn-wrapper">
+                <button >  <a href="{{route('user.contact.us')}}" class="text-decoration-none text-white"    >Get a Quote</a></button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+{{-- new construction --}}
