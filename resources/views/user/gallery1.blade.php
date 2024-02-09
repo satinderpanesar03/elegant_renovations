9ddf6419c5e65d33ec325e2c54334cb51a02e846
Vicky Chhetri <specialpower321@gmail.com>
2024-02-09 22:11:42 +0530

    gallery seo

--- resources/views/user/gallery1.blade.php
+++ resources/views/user/gallery1.blade.php
@@ -1,7 +1,28 @@
 @extends('user.layouts.header')
 
-@section('title', 'Welcome')
+@section('title', 'Gallery')
 
+@push('header_meta_content')
+    <!-- Primary Meta Tags -->
+    <meta name="title" content="Gallery" />
+    <meta name="description" content="From kitchens and bathrooms to living areas and outdoor spaces, find inspiration for every room in your home" />
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://amkahomes.ca/about-us" />
+    <meta property="og:title" content="Gallery" />
+    <meta property="og:description" content="From kitchens and bathrooms to living areas and outdoor spaces, find inspiration for every room in your home" />
+    <meta property="og:image" content="https://amkahomes.ca/user/images/gallery/g2/g2_2.jpg" />
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://amkahomes.ca/about-us" />
+    <meta property="twitter:title" content="Gallery" />
+    <meta property="twitter:description" content="From kitchens and bathrooms to living areas and outdoor spaces, find inspiration for every room in your home" />
+    <meta property="twitter:image" content="https://amkahomes.ca/user/images/gallery/g2/g2_2.jpg" />
+
+    <!-- Meta Tags Generated with https://metatags.io -->
+@endpush
 @section('content')
 
 
