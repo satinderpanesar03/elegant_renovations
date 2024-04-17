f149f1ea647710933799cde573939e4cc3a272b5
satinder <satinderpanesar03@gmail.com>
2024-04-17 22:29:22 +0530

    Signed-off-by: satinder <satinderpanesar03@gmail.com>

--- resources/views/components/user/testimonial2.blade.php
+++ resources/views/components/user/testimonial2.blade.php
@@ -1,31 +1,22 @@
 <div class="mobile-display mt-4">
 
 @foreach($testimonials as $testimonial_note)
-                                        <div class=" swiper-js swiper-slide bg-white h-100 m-1">
-                                            <div class=" swiper-js slide px-4 pt-5 pb-3 border-radius-20px ">
-                                                <div class=" swiper-js  shadow-lg"></div>
-                                                <div class=" swiper-js Customers-profile ">
-                                                    <img src="{{asset('user/images/quotation-mark-icon.png')}}" height="25px" class=" swiper-js quotation">
-                                                </div>
-                                                <div class="swiper-js card-content p-3 ">
-                                                    <div class=" swiper-js text-center mb-md-4">
-                                                        <i class=" swiper-js fa fa-star star-icon-style yellow-star"></i>
-                                                        <i class=" swiper-js fa fa-star star-icon-style yellow-star"></i>
-                                                        <i class=" swiper-js fa fa-star star-icon-style yellow-star"></i>
-                                                        <i class=" swiper-js fa fa-star star-icon-style yellow-star"></i>
-                                                        <i class=" swiper-js fa fa-star star-icon-style yellow-star"></i>
-                                                    </div>
+    <div class="swiper-js card-content p-3 mt-4">
+        <div class=" swiper-js text-center mb-md-4">
+            <i class=" swiper-js fa fa-star star-icon-style yellow-star"></i>
+            <i class=" swiper-js fa fa-star star-icon-style yellow-star"></i>
+            <i class=" swiper-js fa fa-star star-icon-style yellow-star"></i>
+            <i class=" swiper-js fa fa-star star-icon-style yellow-star"></i>
+            <i class=" swiper-js fa fa-star star-icon-style yellow-star"></i>
+        </div>
 
-                                                    <p class=" swiper-js text-center card-desc mb-md-4 p-2 text-wrap  " style="width: 250px;" >
-                                                        "{{$testimonial_note->text}}"</p>
+        <p class=" swiper-js text-center card-desc mb-md-4 p-2 text-wrap  "   >
+        "{{$testimonial_note->text}}"</p>
 
-                                                    <h5 class=" swiper-js text-center fs-20">
-                                                        <b>{{$testimonial_note->name}}.</b>
-                                                    </h5>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    @endforeach
-
+        <h5 class=" swiper-js text-center fs-20">
+            <b>{{$testimonial_note->name}}.</b>
+        </h5>
+    </div>
+@endforeach
 
 </div>
