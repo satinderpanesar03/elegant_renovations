b4e9c4923d37166e8414d86e14e597d640d3d56f
satinder <satinderpanesar03@gmail.com>
2024-04-17 22:44:35 +0530

    Signed-off-by: satinder <satinderpanesar03@gmail.com>

--- resources/views/components/user/testimonial2.blade.php
+++ resources/views/components/user/testimonial2.blade.php
@@ -1,7 +1,7 @@
 <div class="mobile-display mt-4">
 
 @foreach($testimonials as $testimonial_note)
-    <div class="swiper-js card-content p-3 mt-4">
+    <div class="swiper-js card-content p-3 mt-4" style="white-space: nowrap;   overflow: hidden;   text-overflow: ellipsis;">
         <div class=" swiper-js text-center mb-md-4">
             <i class=" swiper-js fa fa-star star-icon-style yellow-star"></i>
             <i class=" swiper-js fa fa-star star-icon-style yellow-star"></i>
@@ -11,7 +11,7 @@
         </div>
 
         <p class=" swiper-js text-center card-desc mb-md-4 p-2 text-wrap  "   >
-        "{{$testimonial_note->text}}"</p>
+        "{{substr($testimonial_note->text, 0, 100) }}"</p>
 
         <h5 class=" swiper-js text-center fs-20">
             <b>{{$testimonial_note->name}}.</b>
